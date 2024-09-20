13a22ee75296f2627584b5fe80c84ab60b164bd4
eunjeeing <ejjeongggg@gmail.com>
2024-09-20 13:57:59 +0900

    :sparkles: Feat: 시스템 메세지 수정

--- app.py
+++ app.py
@@ -81,14 +81,16 @@ def match_products():
         completion = openai.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
-                {"role": "system", "content": "You are a helpful assistant jellbbo. Answer the question truthfully using product"},
+                {"role": "system", "content": "너는 현대식품관 챗봇 젤뽀야. 주어진 상품 데이터가 있으면 관련되어서 답변 해주면 되고, 없으면 일반적인 답변을 해줘. 상품에 대해 너무 자세히 얘기하진 말아줘"},
+                # {"role": "system", "content": "너는 현대식품관 챗봇 젤뽀야. 주어진 상품 데이터가 있으면 관련되어서 답변 해주면 되고, 없으면 일반적인 답변을 해줘. 예를 들어, 냉동 딸기는 그냥 딸기라고만 언급해줘"},
+                # {"role": "system", "content": "You are a helpful assistant jellbbo. Answer the question truthfully using product"},
                 {"role": "assistant", "content": "반가워요, 고객님!\nAI 쇼핑메이트 젤뽀예요 ❤️\n 쇼핑 중 궁금하신 내용을 채팅창에 입력해보세요!"},
                 {"role": "user", "content": "사과 주스 재료를 알려줘"},
                 {"role": "assistant", "content": "사과주스의 재료는 주로 신선한 사과입니다. 추가적으로 물이나 설탕 레몬즙을 넣을 수 있습니다."},
                 {"role": "user", "content": user_prompt}
             ],
-            temperature=0.3,
-            top_p=0.3,
+            temperature=0,
+            top_p=0,
             max_tokens=8000
         )
 
